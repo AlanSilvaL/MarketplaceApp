1470801432106cc1aae3e884b5bee42d8e261549
AlanSilvaL <alan.silva.larios@gmail.com>
2024-09-17 18:46:09 -0600

    Added Custom Control for DetailPage

--- Controls/CustomDetailPageControl.xaml.cs
+++ Controls/CustomDetailPageControl.xaml.cs
@@ -1,50 +1,56 @@
 namespace MarketplaceApp.Controls;
 
-public partial class CustomDetailPageControl : Border
+public partial class CustomDetailPageControl : ContentView
 {
+  
+    public static readonly BindableProperty ProductImageProperty = BindableProperty.Create(
+        nameof(ProductImage), typeof(string), typeof(CustomDetailPageControl), default(string));
 
-    public static readonly BindableProperty ImageProperty = BindableProperty.Create(
-    nameof(Image), typeof(string), typeof(CustomDetailPageControl), default(string));
-
-    public string Image
+    public string ProductImage
     {
-        get => (string)GetValue(ImageProperty);
-        set => SetValue(ImageProperty, value);
+        get => (string)GetValue(ProductImageProperty);
+        set => SetValue(ProductImageProperty, value);
     }
 
-    public static readonly BindableProperty CategoryProperty = BindableProperty.Create(
-        nameof(Category), typeof(string), typeof(CustomDetailPageControl), default(string));
+   
+    public static readonly BindableProperty ProductCategoryProperty = BindableProperty.Create(
+        nameof(ProductCategory), typeof(string), typeof(CustomDetailPageControl), default(string));
 
-    public string Category
+    public string ProductCategory
     {
-        get => (string)GetValue(CategoryProperty);
-        set => SetValue(CategoryProperty, value);
+        get => (string)GetValue(ProductCategoryProperty);
+        set => SetValue(ProductCategoryProperty, value);
     }
 
-    public static readonly BindableProperty TitleProperty = BindableProperty.Create(
-        nameof(Title), typeof(string), typeof(CustomDetailPageControl), default(string));
+    public static readonly BindableProperty ProductTitleProperty = BindableProperty.Create(
+        nameof(ProductTitle), typeof(string), typeof(CustomDetailPageControl), default(string));
 
-    public string Title
+    public string ProductTitle
     {
-        get => (string)GetValue(TitleProperty);
-        set => SetValue(TitleProperty, value);
+        get => (string)GetValue(ProductTitleProperty);
+        set => SetValue(ProductTitleProperty, value);
     }
 
-    public static readonly BindableProperty PriceProperty = BindableProperty.Create(
-    nameof(Price), typeof(decimal), typeof(CustomDetailPageControl), default(decimal));
+    public static readonly BindableProperty ProductPriceProperty = BindableProperty.Create(
+        nameof(ProductPrice), typeof(decimal), typeof(CustomDetailPageControl), default(decimal));
 
-    public decimal Price
+    public decimal ProductPrice
     {
-        get => (decimal)GetValue(PriceProperty);
-        set => SetValue(PriceProperty, value);
+        get => (decimal)GetValue(ProductPriceProperty);
+        set => SetValue(ProductPriceProperty, value);
     }
 
-    public static readonly BindableProperty RatingProperty = BindableProperty.Create(
-        nameof(Rating), typeof(double), typeof(CustomDetailPageControl), default(double));
+    public static readonly BindableProperty ProductRatingProperty = BindableProperty.Create(
+        nameof(ProductRating), typeof(double), typeof(CustomDetailPageControl), default(double));
 
-    public double Rating
+    public double ProductRating
     {
-        get => (double)GetValue(RatingProperty);
-        set => SetValue(RatingProperty, value);
+        get => (double)GetValue(ProductRatingProperty);
+        set => SetValue(ProductRatingProperty, value);
+    }
+
+    public CustomDetailPageControl()
+    {
+        InitializeComponent();
     }
 }
